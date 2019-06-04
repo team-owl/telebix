7cc29a90c2c36cfc18e672c7f972656d40b414ff
Matthew Echert <mechert@tessituranetwork.com>
2019-06-04 12:27:26 -0700

    Added props to AppEvent

--- Telebix/Models/AppEvent.cs
+++ Telebix/Models/AppEvent.cs
@@ -7,5 +7,13 @@ namespace Telebix.Models
 {
     public class AppEvent
     {
+        public DateTime EventDt { get; set; }
+        public int PhoneId { get; set; }
+        public int UserId { get; set; }
+        public string AppVersion { get; set; }
+        public string AndroidVersion { get; set; }
+        public bool Success { get; set; }
+        public float Value { get; set; }
+        public string Message { get; set; }
     }
 }
